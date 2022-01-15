60673ecfc585acf756645891bc18083f41739063
alex <zelenskya@gmail.com>
2022-01-15 23:55:49 +0300

    Creates a simple test (Lesson 13)

--- src/main/java/SeleniumIntroduction.java
+++ src/main/java/SeleniumIntroduction.java
@@ -3,9 +3,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumIntroduction {
     public static void main(String[] args) {
-
+        //driver's version should be up to date with Chrome's one
         System.setProperty("webdriver.chrome.driver","C:\\Users\\User\\Selenium_Course\\chromedriver\\chromedriver.exe");
         //WebDriver is an interface; ChromeDriver implements WebDriver
         WebDriver driver = new ChromeDriver();
+        driver.get("https://www.ozon.ru/");
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        //driver.close();
+        driver.quit();
     }
 }
