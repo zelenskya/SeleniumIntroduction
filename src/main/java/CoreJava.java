364a2b431c4789455bdc1075e0dccf47558aa318
alex <zelenskya@gmail.com>
2022-01-16 22:27:49 +0300

    Adds array, arraylist, string code

---
+++ src/main/java/CoreJava.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class CoreJava {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList<String>();
+        arrayList.add("arrlistitem1");
+    }
+}
--- src/main/java/SeleniumIntroduction.java
+++ src/main/java/SeleniumIntroduction.java
@@ -3,10 +3,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.ArrayList;
+
 public class SeleniumIntroduction {
     public static void main(String[] args) {
         //driver's version should be up to date with Chrome's one
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\User\\Selenium_Course\\chromedriver\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Selenium_Course\\chromedriver\\chromedriver.exe");
         //WebDriver is an interface; ChromeDriver implements WebDriver
         WebDriver driver_chrome = new ChromeDriver();
 
@@ -23,6 +25,22 @@ public class SeleniumIntroduction {
         driver_chrome.get("https://www.ozon.ru/");
         System.out.println(driver_chrome.getTitle());
         System.out.println(driver_chrome.getCurrentUrl());
+
+        String page_title = driver_chrome.getTitle();
+
+        //region Arrays & Strings
+        /*String[] splitted_title = page_title.split(" ");
+        for (int i=0; i<splitted_title.length; i++){
+            System.out.println(splitted_title[i]);
+        };
+
+        ArrayList<String> splitted_title_arr = new ArrayList<String>();
+        for (int i=splitted_title.length-1; i>=0; i--){
+             splitted_title_arr.add(splitted_title[i]);
+        };
+        System.out.println(splitted_title_arr.get(0));*/
+        //endregion
+
         //driver.close();
         driver_chrome.quit();
     }
