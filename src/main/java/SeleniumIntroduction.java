0acdf67fe4c5362cb9e5b73102bb3e0b74c084df
alex <zelenskya@gmail.com>
2022-01-16 12:17:43 +0300

    Adds Firefox and Edge drivers

--- src/main/java/SeleniumIntroduction.java
+++ src/main/java/SeleniumIntroduction.java
@@ -1,16 +1,29 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SeleniumIntroduction {
     public static void main(String[] args) {
         //driver's version should be up to date with Chrome's one
         System.setProperty("webdriver.chrome.driver","C:\\Users\\User\\Selenium_Course\\chromedriver\\chromedriver.exe");
         //WebDriver is an interface; ChromeDriver implements WebDriver
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.ozon.ru/");
-        System.out.println(driver.getTitle());
-        System.out.println(driver.getCurrentUrl());
+        WebDriver driver_chrome = new ChromeDriver();
+
+        //region Firefox
+        //System.setProperty("webdriver.gecko.driver","C:\\Users\\User\\Selenium_Course\\chromedriver\\chromedriver.exe");
+        //WebDriver driver_firefox = new FirefoxDriver();
+        //endregion
+
+        //region Edge
+        //System.setProperty("webdriver.edge.driver", "C:\\Users\\User\\Selenium_Course\\edgedriver\\msedgedriver.exe");
+        //WebDriver driver_edge = new EdgeDriver();
+        //endregion
+
+        driver_chrome.get("https://www.ozon.ru/");
+        System.out.println(driver_chrome.getTitle());
+        System.out.println(driver_chrome.getCurrentUrl());
         //driver.close();
-        driver.quit();
+        driver_chrome.quit();
     }
 }
