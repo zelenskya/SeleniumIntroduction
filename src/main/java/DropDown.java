155bbf8923d741ba1a756146d0d5c625c6753f81
alex <zelenskya@gmail.com>
2022-02-04 16:20:21 +0300

    Working with drop-downs

---
+++ src/main/java/DropDown.java
@@ -0,0 +1,37 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.time.Duration;
+
+public class DropDown {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Selenium_Course\\chromedriver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
+        //region static drop-down, search categories
+        /*driver.get("http://amazon.com");
+        WebElement webeltCategoriesList = driver.findElement(By.id("searchDropdownBox"));
+        Select categoriesList = new Select(webeltCategoriesList);
+        categoriesList.selectByIndex(1);
+        System.out.println(categoriesList.getFirstSelectedOption().getText());
+        Thread.sleep(2000);
+        categoriesList.selectByVisibleText("Books");
+        System.out.println(categoriesList.getFirstSelectedOption().getText());*/
+        //endregion
+
+        //region dynamic drop-down, a number of passengers
+        driver.get("http://aviasales.ru");
+        driver.findElement(By.xpath("//*[@class='container_c4b10b6 additional-fields__dropdown-container']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//div[@data-test-id='passengers-adults-field']//a[contains(@class,'--increment')]")).click();
+        driver.findElement(By.xpath("//div[@data-test-id='passengers-children-field']//a[contains(@class,'--increment')]")).click();
+        driver.findElement(By.xpath("//div[@data-test-id='passengers-infants-field']//a[contains(@class,'--increment')]")).click();
+
+
+    }
+}
