cd4701e58026337fdeadc74d432e49d48ffac666
alex <zelenskya@gmail.com>
2022-02-12 23:05:14 +0300

    Adds check-box

--- src/main/java/DropDown.java
+++ src/main/java/DropDown.java
@@ -27,27 +27,29 @@ public class DropDown {
         //endregion
 
         //region dynamic drop-down, a number of passengers
-        /*driver.get("http://aviasales.ru");
-        driver.findElement(By.xpath("//*[@class='container_c4b10b6 additional-fields__dropdown-container']")).click();
+        driver.get("http://aviasales.ru");
+        /*driver.findElement(By.xpath("//*[@class='container_c4b10b6 additional-fields__dropdown-container']")).click();
         driver.findElement(By.xpath("//div[@data-test-id='passengers-adults-field']//a[contains(@class,'--increment')]")).click();
         //driver.findElement(By.xpath("//div[@data-test-id='passengers-children-field']//a[contains(@class,'--increment')]")).click();
         driver.findElement(By.xpath("(//a[contains(@class,'additional-fields__passenger-control --increment')])[2]")).click();
         driver.findElement(By.xpath("//div[@data-test-id='passengers-infants-field']//a[contains(@class,'--increment')]")).click();
         driver.findElement(By.xpath("//button[@data-test-id='form-submit']")).click();*/
+        System.out.println(driver.findElement(By.xpath("//label[@class='of_input_checkbox__label']")).isEnabled());
+        driver.findElement(By.xpath("//label[@class='of_input_checkbox__label']")).click();
         //endregion
 
         //region context-driven drop-down, search field
-        driver.get("http://amazon.com");
+        /*driver.get("http://amazon.com");
         driver.findElement(By.xpath("//*[@id='twotabsearchtextbox']")).sendKeys("ind");
         Thread.sleep(2000);
         List<WebElement> searchElements = driver.findElements(By.xpath("//div[@class='s-suggestion-container']"));
         for (WebElement item:searchElements){
             if (item.getText().equals("indoor grill")){
                 item.click();
+                break;
             }
         }
-        Thread.sleep(2000);
-        Thread.sleep(2000);
+        Thread.sleep(2000);*/
         //endregion
 
     }
